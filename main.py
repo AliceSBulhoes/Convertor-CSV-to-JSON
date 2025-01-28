92cff91ebf5e8aa6dfc6e1f3a73526523a7d3d9f
Alice <alicesb2006@gmail.com>
2025-01-28 18:27:45 -0300

    Criando a interatividade com o usuario

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Importando bibliotecas
+import os
+import pandas as pd
+import json
 
+# Entrada dos nomes dos arquivos
+file_csv = input("Escreva exatamente o nome do arquivo (sem o .csv): ")
+file_json = input("Escreva o nome do arquivo JSON (sem o .json): ")
 
-
+# Criando os caminhos de forma compatível com o sistema operacional
+csv_path = os.path.join("archive", f"{file_csv}.csv")
+json_path = os.path.join("json", f"{file_json}.json")
+temp_json_path = os.path.join("json", f"temp.json")
